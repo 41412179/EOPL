4a0f2651885c0af9333fa4a14e7cc8fa8633ea53
41412179 <1593028064@qq.com>
2016-09-21 23:14:54 +0800

    eopl version's our-list-ref

--- our-list-ref.scm
+++ our-list-ref.scm
@@ -1,9 +1,12 @@
-#lang racket
+#lang eopl
 ;our-list-ref List n -> nth element 
 (define (our-list-ref lst n)
-  (if (= n 0)
-      (car lst)
-      (if (empty? (cdr lst))
-          "beyond bound"
+  (if (null? lst)
+      (report-list-too-short n)
+      (if (zero? n)
+          (car lst)
           (our-list-ref (cdr lst) (- n 1)))))
-(our-list-ref '(1 '(2 3) 3 4) -1)
+(define (report-list-too-short n)
+  (eopl:error 'our-list-ref
+              "List too short by ~s elements.~%" (+ n 1)))
+(our-list-ref '(1 '(2 3) 3 4) 2)
