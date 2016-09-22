eb062cdeaa53b5e36a6566dad2b4f0f87079ed52
41412179 <1593028064@qq.com>
2016-09-22 10:08:13 +0800

    add remove-all fun

--- remove-first.scm
+++ remove-first.scm
@@ -5,4 +5,11 @@
       '()
       (cond ((equal? element (car lst)) (cdr lst))
             (else (cons (car lst) (remove-first (cdr lst) element))))))
-(remove-first '(1 2 3) 4)
+;(remove-first '(1 2 3) 4)
+;exercise
+(define (remove-all lst element)
+  (if (null? lst)
+      '()
+      (cond ((equal? element (car lst)) (remove-all (cdr lst) element))
+            (else (cons (car lst) (remove-all (cdr lst) element))))))
+(remove-all '(1 2 3 3) 3)
