53a3516a6845b1af0346b3e749073fa19c3b0ebb
41412179 <1593028064@qq.com>
2016-09-22 09:31:03 +0800

    a small fix

--- our-list-ref.scm
+++ our-list-ref.scm
@@ -1,12 +1,19 @@
 #lang eopl
-;our-list-ref: List * Int -> SchemeVal 
+;our-list-ref: List * Int -> SchemeVal
+(define (our-list-ref-v2 lst n)
+  (if (< n 0)
+      (report-list-too-short lst n)
+      (our-list-ref lst n)))
 (define (our-list-ref lst n)
-  (if (null? lst)
-      (report-list-too-short n)
-      (if (zero? n)
-          (car lst)
-          (our-list-ref (cdr lst) (- n 1)))))
-(define (report-list-too-short n)
+  (if (zero? n)
+          (if (null? lst)
+              (report-number-too-big n)
+              (car lst))
+          (our-list-ref (cdr lst) (- n 1))))
+(define (report-list-too-short lst n)
+  (eopl:error 'our-list-ref-v2
+              "List ~s don't hold ~s th elements.~%" lst n ))
+(define (report-number-too-big n)
   (eopl:error 'our-list-ref
-              "List too short by ~s elements.~%" (+ n 1)))
-(our-list-ref '(1 '(2 3) 3 4) -1)
+              "List don't have enough elements~%"))
+(our-list-ref-v2 '(1 '(2 3) 3 4) 1)
